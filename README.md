ee13b392a63c9e2f8c79b5a107459ea0aacb663c
Justin <jus.russell@gmail.com>
2013-12-06 12:12:13 -0500

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# svn-info
+
+Parse svn info.
+
+## Getting Started
+Install the module with: `npm install svn-info`
+
+```javascript
+var svn_info = require('svn-info');
+svn_info.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 jtrussell  
+Licensed under the MIT license.
---
+++ gruntfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    simplemocha: {
+      files: ['test/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'simplemocha']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'simplemocha']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-simple-mocha');
+
+  grunt.registerTask('default', ['jshint', 'simplemocha']);
+
+};
